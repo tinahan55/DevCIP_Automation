5bea29008a703daab6dd0e7777aa11e7ed4eadb4
lileesqa <lileesqa@gmail.com>
2017-03-13 00:50:23 +0800

    Modified OS-1623 script for using Jenkins.

--- test/Other/Issue_Check/OS-1623_SSD.py
+++ test/Other/Issue_Check/OS-1623_SSD.py
@@ -44,48 +44,68 @@ def  check_booting(hostip,check_cycle):
     return k
 
 if __name__ == '__main__':
-    logfilename = "coolboot%s.log"%(strftime("%Y%m%d%H%M", gmtime()))
-    logger = set_log(logfilename,"cool_boot")
-    ip = "10.2.53.163"
-    port = 22
-    mode ="ssh"
-    username = "admin"
-    password ="admin"
-    din_relay_ip = "10.2.53.199"
-    din_relay_user ="root"
-    din_relay_pwd ="lilee1234"
-    din_relay_device_name = "R1-Alpha-STS2"
-    test_cycle = 20000
-    power_cycle_sleep = 180
-    checkcommandlist = ["show interface all","lsblk -l"]
-    checkitemlist = ["maintenance 0 (.*) up","sda | 29.8G"]
-    try:
-        device =Device_Tool(ip,port,mode,username,password,"check_list")
-        powerCycle = powerCycle()
-        if device:
-            device.device_get_version()
-            logger.info("Device Bios Version:%s"%(device.bios_version))
-            logger.info("Device recovery image:%s"%(device.boot_image))
-            logger.info("Device build image:%s"%(device.build_image))
-            for k in range(0, test_cycle):
-                #logger.info("[%s][power_cycle_round]Round :%s"%(k,power_cycle_result))
-                power_cycle_result =powerCycle.powerControl(din_relay_ip, din_relay_user, din_relay_pwd, din_relay_device_name )
-                logger.info("[%s][power_cycle_result]result :%s"%(k,power_cycle_result))
-                if power_cycle_result:
-                    logger.info("[%s][power_cycle_sleep]%s seconds"%(k,power_cycle_sleep))
-                    time.sleep(2)
-                    count = check_booting(ip,power_cycle_sleep)
-                    logger.info("[%s][power_cycle_sleep]wait %s seconds"%(k,count))
-                    if count < power_cycle_sleep:
-                        #time.sleep(power_cycle_sleep)
-                        device =Device_Tool(ip,port,mode,username,password,"check_list")
-                        if device:
-                            checkitem = "device_check_interface_and_mobility"
-                            logger.info("[%s]Starting"%(checkitem))
-                            for index,value in enumerate(checkcommandlist):
-                                checkmatch = checkitemlist[index]
-                                device_check_info(logger,device,checkitem,value,checkmatch)
-    except Exception,ex:
-        logging.error("[coolboot]exception fail:%s "%(str(ex)))
+    mainlogger = Log("OS-1623_ssd", "OS-1623_ssd")
+    if len(sys.argv) > 4:
+        device_info = sys.argv[1].split("_")
+        login_info = sys.argv[2].split("_")
+        din_relay_info = sys.argv[3].split("_")
+        #din_relay_login_info = sys.argv[4].split("_")
+        powercycle_info = sys.argv[4].split("_")
+        device_connect_type = device_info[0]
+        device_ip = device_info[1]
+        device_port = int(device_info[2])
+        username = login_info[0]
+        password = login_info[1]
+        din_relay_ip = din_relay_info[0]
+        din_relay_user = din_relay_info[1]
+        din_relay_pwd = din_relay_info[2]
+        din_relay_device_name = din_relay_info[3]
+        test_cycle = powercycle_info[0]
+        power_cycle_sleep = powercycle_info[1]
+        '''
+                logfilename = "coolboot%s.log"%(strftime("%Y%m%d%H%M", gmtime()))
+                logger = set_log(logfilename,"cold_boot")
+                ip = "10.2.53.163"
+                port = 22
+                mode ="ssh"
+                username = "admin"
+                password ="admin"
+                din_relay_ip = "10.2.53.199"
+                din_relay_user ="root"
+                din_relay_pwd ="lilee1234"
+                din_relay_device_name = "R1-Alpha-STS2"
+                test_cycle = 20000
+                power_cycle_sleep = 180
+                '''
+        checkcommandlist = ["show interface all","lsblk -l"]
+        checkitemlist = ["maintenance 0 (.*) up","sda | 29.8G"]
+        try:
+            device =Device_Tool(device_ip,device_port,device_connect_type,username,password,"check_list")
+            powerCycle = powerCycle()
+            if device:
+                device.device_get_version()
+                #mainlogger.info("Device Bios Version:%s"%(device.bios_version))
+                #mainlogger.info("Device recovery image:%s"%(device.boot_image))
+                #mainlogger.info("Device build image:%s"%(device.build_image))
+                for k in range(0, test_cycle):
+                    #logger.info("[%s][power_cycle_round]Round :%s"%(k,power_cycle_result))
+                    power_cycle_result =powerCycle.powerControl(din_relay_ip, din_relay_user, din_relay_pwd, din_relay_device_name )
+                    #mainlogger.info("[%s][power_cycle_result]result :%s"%(k,power_cycle_result))
+                    if power_cycle_result:
+                        #mainlogger.info("[%s][power_cycle_sleep]%s seconds"%(k,power_cycle_sleep))
+                        time.sleep(2)
+                        count = check_booting(device_ip,power_cycle_sleep)
+                        #mainlogger.info("[%s][power_cycle_sleep]wait %s seconds"%(k,count))
+                        if count < power_cycle_sleep:
+                            #time.sleep(power_cycle_sleep)
+                            device =Device_Tool(device_ip,device_port,device_connect_type,username,password,"check_list")
+                            if device:
+                                checkitem = "device_check_interface_and_mobility"
+                                #mainlogger.info("[%s]Starting"%(checkitem))
+                                for index,value in enumerate(checkcommandlist):
+                                    checkmatch = checkitemlist[index]
+                                    device_check_info(mainlogger,device,checkitem,value,checkmatch)
+        except Exception,ex:
+            logging.error("[coolboot]exception fail:%s "%(str(ex)))
 
 
