2bcdf0050d39ae1e66d738076433a1285248340e
lileesqa@gmail.com <sqalilee1234>
2018-03-10 00:19:54 +0800

    Update "sudo" as pattern for app-engine OS using

--- lib/SSHConsole.py
+++ lib/SSHConsole.py
@@ -55,7 +55,7 @@ class SSHConnect(object):
         time.sleep(2)
         response_result =  self.remote_conn.recv(5000)
         if mode == "shell":
-            if "#" in response_result or "$" in response_result: # "$" Use for ssh to app-engine
+            if "#" in response_result or "$" in response_result or "[sudo] password for" in response_result: # "$" Use for ssh to app-engine
                 mode_result = True
             elif "localdomain" in response_result:
                 self.remote_conn.send("%s\n"%("diag shell"))
